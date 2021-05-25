93e92757494993d87dd53b4e35f501a409a09115
mayukh-B <rudrabagchi11@gmail.com>
2021-05-26 00:34:48 +0530

    shrinking feature added

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const ejs=require('ejs');
-
 const mongoose=require('mongoose');
-
+const ShortUrl =require('./models/shortUrl');
+mongoose.connect('mongodb://localhost/urlShrinker',{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+})
 
 const app = express();
 
 
 app.set('view engine','ejs');
-
+app.use(express.urlencoded({extended:false}))
 
 app.get("/",function(req,res){
     res.render("index")
 })
 
-
+app.post("/shortUrls", async(req,res)=>{
+ await   ShortUrl.create({
+        full:req.body.fullUrl})
+        res.redirect('/');
+})
 
 
 
